d4c03e27bd37941dcf1b7e034788ddb5b7e80278
Okere-NP <okereprince360@gmail.com>
2024-06-17 04:43:45 +0100

    GameMain Adjustment

--- src/GameMain.jl
+++ src/GameMain.jl
@@ -2,32 +2,36 @@
     ***WELCOME TO THE GAME OF ROCK-PAPER-SCISSORS***
 =#
 
-include("GameFunctions.jl");
+begin   # To automatically run the Game logic. . .
+    include("GameFunctions.jl") # Import GameFunctions.jl to GameMain.jl
 
-# A quick game guide:
-print(
-    "Rules in winning the game are:\n"
-    * "Rock vs Paper -> Paper wins because it covers Rock\n"
-    * "Paper vs Scissors -> Scissors wins because it cuts Paper\n"
-    * "Scissors vs Rock -> Rock wins because it smashes Scissors\n"
-)
+    while true
+        println("W E L C O M E\n")
+        # A quick game guide:
+        print(
+            "Rules in winning the game are:\n"
+            * "Rock vs Paper -> Paper wins because it covers Rock\n"
+            * "Paper vs Scissors -> Scissors wins because it cuts Paper\n"
+            * "Scissors vs Rock -> Rock wins because it smashes Scissors\n"
+        )
 
-while true
-    try
-        get_players_action()    # Function to get user and computer selection...
-        winner_determination()  # Function to determine who won or lost...
-    catch
-        @warn "You entered an invalid option, Kindly try again..."
-        continue # Get back to game logic...
-    end
+        try
+            player_action = get_players_action()    # Function to get user and computer selection...
+            winner_determination(player_action)  # Function to determine who won or lost...
+        catch
+            @warn "You entered an invalid option, Kindly try again..."
+            continue # Get back to game logic...
+        end
 
-    print("\nDo you want to play again?\n"
-    * "Press (y for Yes) or (n for No)\n"
-    * "Enter choice: ")
-    user_choice = readline()
+        print("\nDo you want to play again?\n"
+        * """Press (y for Yes) or (n for No, or press "Enter" to Exit)\n"""
+        * "Enter choice: ")
+        user_choice = readline()
 
-    if user_choice != "y" && uppercase(user_choice) != "Y"  # Exit game if user select No(n)...
-        break
+        if user_choice != "y" && uppercase(user_choice) != "Y"  # Exit game if user select No(n)...
+            break
+        end
+
     end
 
 end
